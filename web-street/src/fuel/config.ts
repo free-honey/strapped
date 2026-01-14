a3b97553fbdde7737c90bdbd62c65d2e3d6447a4
Mitch Turner <james.mitchell.turner@gmail.com>
2026-01-14 12:41:33 -0700

    Fix hard-coded contract id

--- web-street/src/fuel/config.ts
+++ web-street/src/fuel/config.ts
@@ -3,7 +3,7 @@ export const FUEL_NETWORKS = {
     label: "Testnet",
     graphqlUrl: "https://testnet.fuel.network/v1/graphql",
     contractId:
-      "bfd7d193d76ba362034a60276201adb83540b11b4e98ee69df24309a883db9c8",
+      "0xbfd7d193d76ba362034a60276201adb83540b11b4e98ee69df24309a883db9c8",
     chipAssetId:
       "0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07",
   },
